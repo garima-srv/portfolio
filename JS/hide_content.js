1d0309a7146bad68102010be8dbaf767e5ca58c6
Brijesh <brijeshyadavjthj@gmail.com>
2021-07-12 15:26:33 +0530

    added coding is lub in footer and view less or more on click in projects functionality added

--- JS/hide_content.js
+++ JS/hide_content.js
@@ -1,3 +1,23 @@
+
+$('.hide-content').toggle("slow");
+
+var clicked=1;
+
 $('#hide-button').click(function(){
     $('.hide-content').toggle("slow");
+
+    clicked=clicked+1;
+
+    if(clicked%2==0)
+    {
+        $(this).html('<a id="button-content">View  Less  Projects &#x21E1;');
+    }
+    else
+    {
+        $(this).html('<a id="button-content">View  More  Projects #x21E3;');  
+    }
+
+    e.preventDefault();
+
+
 })
